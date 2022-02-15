700bd6cec4eff852f58ad700246a0b9743be7219
amanvad <99163608+amanvad@users.noreply.github.com>
2022-02-15 09:53:31 -0600

    Update UsingPython.py

--- UsingPython.py
+++ UsingPython.py
@@ -2,13 +2,19 @@ from urllib.request import urlopen
 
 log_raw_file = urlopen("https://s3.amazonaws.com/tcmg476/http_access_log")
 
-log_file = log_raw_file.read()
+log_read = log_raw_file.read()
+
+log_file = open("log_read", "r")
+
+log_file = log_file.split("\n")
 
 log_total = len(log_file)
 
+
+print(log_file[0])
 
 print("This is how many total requests were made in the past 6 months: ",)
 
 print("This is how many total requests were made in the time period: ", log_total)
 
-print(log_file[0])
+
