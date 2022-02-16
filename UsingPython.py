da364eff3265cca5e64481af961f525a895b3227
amanvad <99163608+amanvad@users.noreply.github.com>
2022-02-15 22:27:00 -0600

    Update UsingPython.py

--- UsingPython.py
+++ UsingPython.py
@@ -9,13 +9,13 @@ log_total = len(split_file)
 
 six_month_start = False
 six_month_total = 0
-            
+
 for log in split_file:
     if "11/Apr/1995:00:00:16" in log:
         six_month_start = True
     if six_month_start == True:
         six_month_total += 1
-        
-print(\n)
+
+print('\n')
 print("This is how many total requests were made in the past 6 months: ", six_month_total)
 print("This is how many total requests were made in the time period: ", log_total)
