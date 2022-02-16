674e4b072ae6e9e7cab919c21385b04cee205112
amanvad <99163608+amanvad@users.noreply.github.com>
2022-02-15 23:01:23 -0600

    Update UsingPython.py

--- UsingPython.py
+++ UsingPython.py
@@ -1,16 +1,19 @@
+import os.path
 import urllib.request
 
-print("Downloading file...")
+if os.path.isfile("get_file") == False:
+    print('\n')
+    print("Downloading file...")
+    get_file = urllib.request.urlretrieve("https://s3.amazonaws.com/tcmg476/http_access_log", "get_file")
+    
 print('\n')
 print("Counting requests...")
-
-get_file = urllib.request.urlretrieve("https://s3.amazonaws.com/tcmg476/http_access_log", "get_file")
+    
 open_file = open("get_file", "r")
 read_file = open_file.read()
 split_file = read_file.split('\n')
 
 log_total = len(split_file)
-
 six_month_start = False
 six_month_total = 0
 
@@ -23,4 +26,3 @@ for log in split_file:
 print('\n')
 print(f"There were {six_month_total} requests made in the past 6 months.")
 print(f"There were {log_total} requests made in the entire time period.")
-print('\n')
