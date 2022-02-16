c5ace0e01da575af1ca8149033c126633b11a6fc
amanvad <99163608+amanvad@users.noreply.github.com>
2022-02-15 22:14:38 -0600

    Update UsingPython.py

--- UsingPython.py
+++ UsingPython.py
@@ -1,23 +1,9 @@
 import urllib.request
 
-# log_raw_file = urlopen("https://s3.amazonaws.com/tcmg476/http_access_log")
-# log_read = log_raw_file.read()
-# log_file = open("log_read", "r")
-# with urllib.request.urlopen("https://s3.amazonaws.com/tcmg476/http_access_log") as open_log:
-#   log_file = open_log.read()
-# with open("open_file") as log_file:
-    # log_list = list(log_file)
-   
-
-   
-
 get_file = urllib.request.urlretrieve("https://s3.amazonaws.com/tcmg476/http_access_log", "get_file")
-
 open_file = open("get_file", "r")
-
 read_file = open_file.read()
-
-split_file = read_file.split("\n")
+split_file = read_file.split(\n)
 
 log_total = len(split_file)
 
@@ -29,11 +15,9 @@ for log in split_file:
         six_month_start = True
     if six_month_start == True:
         six_month_total += 1
-            
+        
+print(/n)            
 print("This is how many total requests were made in the past 6 months: ", six_month_total)
-
-print('/n')
-
 print("This is how many total requests were made in the time period: ", log_total)
 
 
